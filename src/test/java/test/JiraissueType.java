650e8bb594dde348662a479d0bd7a669640a5032
Curry Pham <64634056+CurryPham@users.noreply.github.com>
2023-01-28 08:22:24 +0700

    JIRA API - Extract response with JSONPath

--- src/test/java/test/JiraissueType.java
+++ src/test/java/test/JiraissueType.java
@@ -1,31 +1,15 @@
 package test;
 
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import model.RequestCapability;
-import org.apache.commons.codec.binary.Base64;
-
-import static io.restassured.RestAssured.given;
+import utils.ProjectInfo;
 
 public class JiraissueType implements RequestCapability {
 
     public static void main(String[] args) {
         String baseUri = "https://restassuredapi.atlassian.net";
-        String pathUrl = "/rest/api/3/project/RA";
+        String projectKey = "RA";
 
-        String email = "khoapd2000@gmail.com";
-        String apiToken = "r4PV3w5mfeQ26Q4xhSfvF2AF";
-        String cred = email.concat(":").concat(apiToken);
-        byte[] encodedCred = Base64.encodeBase64(cred.getBytes());
-        String encodedCredStr = new String(encodedCred);
-
-        RequestSpecification request = given();
-        request.baseUri(baseUri);
-        request.header(defaultHeader);
-        request.header(getAuthenticatedHeader.apply(encodedCredStr));
-
-        Response response = request.get(pathUrl);
-        response.prettyPrint();
-
+        ProjectInfo projectInfo = new ProjectInfo(baseUri, projectKey);
+        System.out.println("Task ID: " + projectInfo.getIssueTypeId("task"));
     }
 }
---
+++ src/test/java/utils/ProjectInfo.java
@@ -0,0 +1,66 @@
+package utils;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import model.RequestCapability;
+import org.apache.commons.codec.binary.Base64;
+
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class ProjectInfo implements RequestCapability {
+
+    private String baseUri;
+    private String projectKey;
+    private List<Map<String,String>> issueTypes;
+    private Map<String, List<Map<String, String>>> projectInfo;
+
+    public ProjectInfo(String baseUri, String projectKey) {
+
+        this.baseUri = baseUri;
+        this.projectKey = projectKey;
+        getProjectInfo();
+    }
+
+    private void getIssueTypes(){
+        issueTypes = projectInfo.get("issueTypes");
+    }
+
+    public String getIssueTypeId(String issueTypeStr) {
+        getIssueTypes();
+
+        String issueTypeId = null;
+        for (Map<String, String> issueType : issueTypes) {
+            if (issueType.get("name").equalsIgnoreCase(issueTypeStr)) {
+                issueTypeId = issueType.get("id");
+                break;
+            }
+        }
+
+        if ( issueTypeId == null ) {
+            throw new IllegalArgumentException("[ERR] Could not find the is for " + issueTypeStr);
+        }
+        return issueTypeId;
+    }
+
+    private void getProjectInfo(){
+        String pathUrl = "/rest/api/3/project/" .concat(projectKey);
+
+        String email = "khoapd2000@gmail.com";
+        String apiToken = "XMeWq1JPiEqBlba3oXPU6DE0";
+        String cred = email.concat(":").concat(apiToken);
+        byte[] encodedCred = Base64.encodeBase64(cred.getBytes());
+        String encodedCredStr = new String(encodedCred);
+
+        RequestSpecification request = given();
+        request.baseUri(baseUri);
+        request.header(defaultHeader);
+        request.header(getAuthenticatedHeader.apply(encodedCredStr));
+        Response response = request.get(pathUrl);
+        projectInfo = JsonPath.from(response.asString()).get();
+    }
+}
+
